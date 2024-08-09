4716888caabe24edb4d759b5a1dc1cb289703721
Anastasiia Turchenkova <artemichevaanastasya@gmail.com>
2024-08-09 18:55:16 -0400

    екн ашч

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 ﻿export default {
     base: '/test_task_vite/',
+    //скопировала из стековерфлоу тут https://stackoverflow.com/questions/72618944/get-error-to-build-my-project-in-vite-top-level-await-is-not-available-in-the
+    build: {
+        target: 'esnext' //browsers can handle the latest ES features
+    }
 }
